25ee5732c23177127498f87082d8d448b6ccfbf5
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-05-19 10:47:48 +0300

    dev commit

--- js/components/clock/Clock.js
+++ js/components/clock/Clock.js
@@ -1,31 +1,7 @@
-/*function Clock() {
-const HTML = `<div class="time">
-                <div class="value">432</div>
-                <div class="label">Days</div>
-            </div>                                                                  ILGASIS BUDAS
-            <div class="time">
-                <div class="value">09</div>
-                <div class="label">Hours</div>
-            </div>
-            <div class="time">
-                <div class="value">37</div>
-                <div class="label">Minutes</div>
-            </div>
-            <div class="time">
-                <div class="value">39</div>
-                <div class="label">Seconds</div>
-            </div>`;
-    const selector = `.clock`;
-    const DOM = document.querySelector(selector);
-
-    DOM.innerHTML = HTML;
-}
-*/
-
 function Clock(selector) {
     const DOM = document.querySelector(selector);
     const timeValues = [432, 9, 37, 39];
-    const labelValues = ['Days', 'Hours', 'Minutes', 'Seconds'];                 //GREITASIS BUDAS
+    const labelValues = ['Days', 'Hours', 'Minutes', 'Seconds'];
     let HTML = '';
 
     for (let i = 0; i < timeValues.length; i++) {
