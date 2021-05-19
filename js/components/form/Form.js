9aecae71548cbf14950bf0f5f96f56e9776d77b4
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-05-19 11:35:21 +0300

    With form

--- js/components/form/Form.js
+++ js/components/form/Form.js
@@ -1,1 +1,1 @@
-
+export { Form }
--- js/home.js
+++ js/home.js
@@ -1,5 +1,7 @@
 // IMPORT
 import { Clock } from "./components/clock/Clock.js";
+import { Form } from "./components/form/Form.js";
 
 // EXECUTION
-Clock('#clock_1');
+Clock ('#clock_1');
+Form ('#form_1');
